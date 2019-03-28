f1f5a918cc6da6f97cf0bf51a413680fc6618ae8
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-03-27 21:26:43 -0400

    updated route files to export routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,26 @@
 var db = require("../models");
 
-module.exports = function(app) {
+//Routes object
+
   // Get all examples
-  app.get("/api/examples", function(req, res) {
+  routes.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
+  routes.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  routes.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
     });
   });
-};
+
+  module.exports = routes;
