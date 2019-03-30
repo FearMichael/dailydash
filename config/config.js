eadf0ce02868c693871d77f2203102822e6f3661
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-03-30 16:10:57 -0400

    creating endpoint for OAuth

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DBUSER,
-    "password": process.env.DBPASS,
-    "database": process.env.DBNAME,
-    "host": process.env.DBHOST,
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "testdb",
-    "host": "localhost",
-    "dialect": "mysql",
-    "logging": false
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
-  }
+    "development": {
+        "username": process.env.DBUSER,
+        "password": process.env.DBPASS,
+        "database": process.env.DBNAME,
+        "host": process.env.DBHOST,
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "testdb",
+        "host": "localhost",
+        "dialect": "mysql",
+        "logging": false
+    },
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql"
+    }
 
 };
