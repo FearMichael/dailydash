182a64724d233344a83472dce545cdd92007833d
Starry Night <44407940+FearMichael@users.noreply.github.com>
2019-04-06 10:05:09 -0400

    route update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(passport.initialize());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/authenticate"
+    callbackURL: "/authenticate"
 },
 function(accessToken, refreshToken, profile, done) {
     // URLSearchParams.findOrCreate({googleId: profile.id}, function(err, user) {
