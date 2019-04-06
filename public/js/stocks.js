d15c94f91030a18dfe8f9b0a3d722cae89571f4e
sip615 <sunilpatel.328@gmail.com>
2019-04-05 21:07:24 -0400

    commit stocks.js

---
+++ public/js/stocks.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#stock").on("click", function() {
+        $(".card-body").css("display","block");
+        $(".stock").hide();
+        $("#stock").hide();
+        // $(".card").css("display", "block");
+        $.get("/stock", function(data) {
+            addNews = $(".n");
+            addNews.empty();
+            $(".stock_lookup").hide();
+            for (var i = 0; i < 5; i++) {
+                addNews.append("<strong>" + data.articles[i].title + "</strong><br>");
+                addNews.append("<a href='" + data.articles[i].url + "'>" + data.articles[i].source.name + "</a><hr>");
+            }
+        });
+    });
+
+});
+console.log("Stocks reached");
+
+$("#stock").on("click", function() {
+    let symbol = $("quote_lookup").val()
+    $.post("/stock", {stock: symbol}, function(stocks) {
+        console.log(stocks);
+        $("#stockInfo").empty();
+
+        stocks.result.forEach(function(){
+            $("#stock").append(`Symbol: ${quoteResponse.result[0].symbol}
+            `)
+                .append (`Price: ${quoteResponse.result[0].regularMarketPrice}
+            `)
+                .append (`Time: ${quoteResponse.result[0].regularMarketTime}
+            `);
+        });
+    });
+});
